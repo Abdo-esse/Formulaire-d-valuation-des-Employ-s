92a8a294608b12986e8764cda2002e6f14fa26fd
abdelilah2005 <abdelilahessemlali@gmail.com>
2024-10-31 10:13:28 +0100

    J'ai commencer le travalier sur la fonction du validation

--- script.js
+++ script.js
@@ -23,20 +23,29 @@ function masquer3() {
     document.querySelectorAll(".bto").forEach(s=>{s.style.display="none"
     })
     document.getElementById("envoyer").style.display = "block";
+  }
    
+  let  firstName = document.querySelector("name='firstName'")
+  let  lastName= document.querySelector("name='lastName'")
+  let  Address = document.querySelector("name='Address'")
+  let  City = document.querySelector("name='City'")
+  let  Check = document.querySelector("name='Check'")
+  
+
+  document.forms[0].onsubmit = fonction (e) {
+    let firstVali = false;
+    let lastVali = false;
+    let Addressvali = false
+    let  Cityvali = false
+    let  Checkvali = false
+    
 
 
+    if (firstVali === false || lastVali === false ||Addressvali === false || Cityvali === false || Checkvali === false ){
+      e.preventDefault();
+    }
 
   }
 
 
 
-//   const valid = document.querySelector("#gridCheck")
-//   const valibtn =()=>{
-    
-//     if (valid) {
-//         console.log('valid');
-        
-        
-//     }
-//   }

a56582f6a9fb5fefebf01c41d7a3be10ea3625d3
abdelilah2005 <abdelilahessemlali@gmail.com>
2024-10-31 13:04:25 +0100

    J'ai termine la fonction de validation de premier section

--- script.js
+++ script.js
@@ -1,51 +1,69 @@
 
-function masquer1() {
-  document.getElementById("per").style.display = "none";
+function masquer2() {
+  document.getElementById("pro").style.display = "none";
+  document.getElementById("per").style.display = "block";
+}
+function masquer3() {
+  document.getElementById("pro").style.display = "none";
+  document.getElementById("com").style.display = "block";
+}
+function masquer4() {
+  document.getElementById("com").style.display = "none";
   document.getElementById("pro").style.display = "block";
 }
-function masquer2() {
-    document.getElementById("pro").style.display = "none";
-    document.getElementById("per").style.display = "block";
-  }
-function masquer3() {
-    document.getElementById("pro").style.display = "none";
-    document.getElementById("com").style.display = "block";
-  }
-  function masquer4() {
-    document.getElementById("com").style.display = "none";
-    document.getElementById("pro").style.display = "block";
-  }
-  function continu() {
-    document.getElementById("per").style.display = "block";
-    document.getElementById("pro").style.display = "block";
-    document.getElementById("com").style.display = "block";
-    
-    document.querySelectorAll(".bto").forEach(s=>{s.style.display="none"
-    })
-    document.getElementById("envoyer").style.display = "block";
-  }
-   
-  let  firstName = document.querySelector("name='firstName'")
-  let  lastName= document.querySelector("name='lastName'")
-  let  Address = document.querySelector("name='Address'")
-  let  City = document.querySelector("name='City'")
-  let  Check = document.querySelector("name='Check'")
-  
+function continu() {
+  document.getElementById("per").style.display = "block";
+  document.getElementById("pro").style.display = "block";
+  document.getElementById("com").style.display = "block";
 
-  document.forms[0].onsubmit = fonction (e) {
-    let firstVali = false;
-    let lastVali = false;
-    let Addressvali = false
-    let  Cityvali = false
-    let  Checkvali = false
-    
+  document.querySelectorAll(".bto").forEach((s) => {
+    s.style.display = "none";
+  });
+  document.getElementById("envoyer").style.display = "block";
+}
 
+let first_Name = document.querySelector("[name='firstName']");
 
-    if (firstVali === false || lastVali === false ||Addressvali === false || Cityvali === false || Checkvali === false ){
-      e.preventDefault();
-    }
+let last_Name = document.querySelector("[name='lastName']");
 
+let Add_ress = document.querySelector("[name='Address']");
+
+let Ci_ty = document.querySelector("[name='City']");
+
+let Ch_eck = document.querySelector("[name='Check']");
+
+document.querySelector("[name='btn']").onclick = function (e) {
+  let firstVali = false;
+  let lastVali = false;
+  let Addressvali = false;
+  let Cityvali = false;
+  // let  Checkvali = false
+
+  if (first_Name.value !== "" && first_Name.value.length <= 10) {
+     firstVali = true;
+  }
+  if (last_Name.value !== "" && last_Name.value.length <= 10) {
+    lastVali = true;
+  }
+  if (Add_ress.value !== "" && Add_ress.value.length > 10) {
+    Addressvali = true;
   }
 
+  if (Ci_ty.value !== "" && Ci_ty.value.length <= 10) {
+    Cityvali = true;
+    console.log("valid");
+   }
+
+  if (
+    firstVali === false || lastVali === false || Addressvali === false || Cityvali === false ) {
+    e.preventDefault();
+   }
+  if (
+    firstVali === true && lastVali === true && Addressvali === true && Cityvali === true) {
+    document.getElementById("per").style.display = "none";
+  document.getElementById("pro").style.display = "block";
+  }
+
+};
 
 
